eacab4573dcf32d49b9f23c3cddc147147fadfc7
lllll963247 <lllll963247@yahoo.com.tw>
2019-05-07 15:44:41 +0800

    Add files via upload

--- catch.py
+++ catch.py
@@ -3,10 +3,23 @@ import requests
 re = requests.get("https://rate.bot.com.tw/xrt?Lang=zh-TW")  
 sp = BeautifulSoup(re.text, 'html.parser')
 
+#print('臺灣銀行')
+
 time = sp.select('.time')
 
 print(time[0].string)
 
 #--------------------------
 
-#bank = sp.sele
+cash = sp.select('.rate-content-cash')
+
+#日幣
+print(cash[17].string)    #現金買入
+print(cash[18].string)    #現金賣出	
+
+sight = sp.select('.rate-content-sight')
+
+print(sight[17].string)    #即期買入	
+print(sight[18].string)    #即期賣出
+
+
